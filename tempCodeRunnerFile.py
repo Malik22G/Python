ab8085b22a63f8221e44efce587cee6c104243de
Malik22G <malik.basit3690@gmail.com>
2024-10-04 16:20:14 +0200

    latest commit

--- tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-def mul(lhs_digits: list[int], rhs_digits: list[int], base: int):
-#     len = max(len(lhs_digits,rhs_digits))
-#     result = [0]* len
-#     carry = 0
-    
-
-        
-    
-#     return result
